4a8d95dd314e2d5edaa5ba06dad0d89274053488
Nubah Uddin <nubahud@umich.edu>
2025-10-06 00:17:10 -0400

    Implementing a method to create a data dictionary from the csv file

--- project1.py
+++ project1.py
@@ -21,7 +21,30 @@ class Project_1:
         'sex': [],
         'year': []
     }
-    
+    def __str__(self):
+        return f"{self.data}"
+
+    def make_data_dict(self):
+
+        with open(self.full_path) as csvfile:
+            reader = csv.reader(csvfile)
+            header = next(reader)
+            for row in reader:
+                if (row[1] != "NA" and row[2] != "NA"
+                and row[3] != "NA"
+                and row[4] != "NA"
+                and row[5] != "NA"
+                and row[6] != "NA"
+                and row[7] != "NA"
+                and row[8] != "NA"):
+                    self.data['species'].append(row[1])
+                    self.data['island'].append(row[2])
+                    self.data['bill_length_mm'].append(float(row[3]))
+                    self.data['bill_depth_mm'].append(float(row[4]))
+                    self.data['flipper_length_mm'].append(int(row[5]))
+                    self.data['body_mass_g'].append(int(row[6]))
+                    self.data['sex'].append(row[7])
+                    self.data['year'].append(int(row[8]))
     
     def species_bill_length(self):
         
